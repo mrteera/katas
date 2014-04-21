a31a0080aff44ead9df8ff1711a02ed6dbd8578a
mrteera <webmaster@mrteera.com>
2014-04-21 14:12:29 +0700

    Timeout

--- python3/prime_factors/prime_factors.py
+++ python3/prime_factors/prime_factors.py
@@ -1,36 +1,16 @@
-class PrimeFactors:
+class PrimeFactor:
 
-    def __init__(self):
-        number = 0
+    def put(self, num):
+        return self.prime_factors(num)
 
-    def set_number(self, number):
-        self.number = number
-
-    def factor(self, number, list_number):
-        if not number % 2:
-            number /= 2
-            list_number.append(2)
-        elif not number %3:
-            number /= 3
-            list_number.append(3)
+    def prime_factors(self, num):
+        result = []
+        while num > 3 and num % 2 == 0:
+            num = num // 2
+            result.append(num)
+        if num == 1:
+            return result
         else:
-            return self.factor(number, list_number)
-
-    def prime(self):
-        if self.number % 2 != 0 and self.number % 3 != 0:
-            results = [self.number]
-        else:
-            results = []
-        if self.number == 1:
-            return []
-        list_number = []
-        results = self.factor(self.number, list_number)
-
-#        while self.number % 2 == 0 :
-#            results.append(2)
-#            self.number /= 2
-#        while self.number % 3 == 0:
-#            results.append(3)
-#            self.number /= 3
-        return results
+            result = [num]
+        return result
 
--- python3/prime_factors/test_prime_factors.py
+++ python3/prime_factors/test_prime_factors.py
@@ -1,43 +1,29 @@
 import unittest
 
-from prime_factors import PrimeFactors
+from prime_factors import PrimeFactor
 
-class testPrimeFactors(unittest.TestCase):
-
-    def setUp(self):
-        self.prime_factors = PrimeFactors()
+class TestPrimeFactor(unittest.TestCase):
 
     def test_1_should_return_empty_list(self):
-        self.prime_factors.set_number(1)
-        actual = self.prime_factors.prime()
+        self.prime_factors = PrimeFactor()
+        actual = self.prime_factors.put(1)
         expected = []
         self.assertEqual(expected, actual)
 
     def test_2_should_return_list_of_2(self):
-        self.prime_factors.set_number(2)
-        actual = self.prime_factors.prime()
+        self.prime_factors = PrimeFactor()
+        actual = self.prime_factors.put(2)
         expected = [2]
         self.assertEqual(expected, actual)
 
     def test_3_should_return_list_of_3(self):
-        self.prime_factors.set_number(3)
-        actual = self.prime_factors.prime()
+        self.prime_factors = PrimeFactor()
+        actual = self.prime_factors.put(3)
         expected = [3]
         self.assertEqual(expected, actual)
 
     def test_4_should_return_list_of_2_2(self):
-        self.prime_factors.set_number(4)
-        actual = self.prime_factors.prime()
-        expected = [2, 2]
-        self.assertEqual(expected, actual)
-
-    def test_5_should_return_list_of_5(self):
-        self.prime_factors.set_number(5)
-        actual = self.prime_factors.prime()
-        expected = [5]
-        self.assertEqual(expected, actual)
-    def test_6_should_return_list_of_2_3(self):
-        self.prime_factors.set_number(6)
-        actual = self.prime_factors.prime()
-        expected = [2,3]
+        self.prime_factors = PrimeFactor()
+        actual = self.prime_factors.put(4)
+        expected = [2,2]
         self.assertEqual(expected, actual)
