eeadbab1a36ff18dcde9c20ad0d1a570a02c5841
mrteera <webmaster@mrteera.com>
2014-05-12 10:06:34 +0700

    Pass the second test!

--- python3/prime_factors/prime_factors.py
+++ python3/prime_factors/prime_factors.py
@@ -1,4 +1,9 @@
 class PrimeFactors:
 
-    def put(self):
-        return []
+    def put(self, num):
+        result = []
+        while num > 1:
+            result.append(2)
+            num = num - 1
+
+        return result
--- python3/prime_factors/test_prime_factors.py
+++ python3/prime_factors/test_prime_factors.py
@@ -7,5 +7,11 @@ class TestPrimeFactors(unittest.TestCase):
     def test_1_should_return_empty_list(self):
         self.prime_factors = PrimeFactors()
         expected = []
-        actual = self.prime_factors.put()
+        actual = self.prime_factors.put(1)
+        self.assertEqual(expected, actual)
+
+    def test_2_should_return_list_of_2(self):
+        self.prime_factors = PrimeFactors()
+        expected = [2]
+        actual = self.prime_factors.put(2)
         self.assertEqual(expected, actual)
