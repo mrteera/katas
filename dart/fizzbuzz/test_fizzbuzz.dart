06bb5c42b3523221f414aa3aa6643fea7c3898d9
mrteera <webmaster@mrteera.com>
2014-10-29 12:28:49 +0700

    Refactor tests, workaround for setUp()

--- dart/fizzbuzz/test_fizzbuzz.dart
+++ dart/fizzbuzz/test_fizzbuzz.dart
@@ -2,14 +2,14 @@ import 'package:unittest/unittest.dart';
 import 'fizzbuzz.dart';
 
 main() {
+ var fizzbuzz = new FizzBuzz();
+
  test('one should return one', () {
-    var fizzbuzz = new FizzBuzz();
     fizzbuzz.set(1);
     expect(fizzbuzz.get(), 1);
   });
 
   test('two should return two', () {
-    var fizzbuzz = new FizzBuzz();
     fizzbuzz.set(2);
     expect(fizzbuzz.get(), 2);
   });
